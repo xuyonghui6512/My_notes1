9166a289d9fc8d1f7e5a51aa0f19fd2cd63e3600
xuyonghui6512 <1244885997@qq.com>
2018-03-27 20:52:03 +0800

    s

---
+++ 条形码识别/include/find_and_pick.h
@@ -0,0 +1,15 @@
+/*2018.3.27
+  made by Xuxuxu
+  This class contains contents about how to 
+  find the code form a picuture we have provided
+  and pick up its ifo.
+*/ 
+#ifndef FIND_AND_PICK_H
+#define FIND_AND_PICK_H
+
+#include "common_include.h"
+
+//new include needed to use Zbar
+#include <zbar.h>
+using namespace zbar;       
+    
