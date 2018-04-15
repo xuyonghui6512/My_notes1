6287ec0e34143850c3b7a00eb01ba803d355c799
xuyonghui6512 <1244885997@qq.com>
2018-04-15 21:58:37 +0800

    gai

---
+++ 多线程练习/build/CMakeFiles/multithreading.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multithreading.dir/main.cpp.o"
+  "../bin/multithreading.pdb"
+  "../bin/multithreading"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multithreading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
